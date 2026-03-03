556081bd347732e72bbbf005f62c3925e2f49506
nwgeishofski <nmwgeishofski1@students.everettcc.edu>
2026-03-03 00:15:38 +0000

    saving notes

---
+++ debugging notes.py
@@ -0,0 +1,19 @@
+import math
+# Four most common tpyes of errors in the stack trace
+
+# 1. Syntax error
+# print(hello world")
+
+# 2. Type error
+#a = 9
+#b = "apple"
+
+#sum = a + b
+
+# 3. Name error
+car_speed = 0
+car_speed = 100
+# print(car_values)
+
+# 4. Value error
+#math.sqrt(-10)
